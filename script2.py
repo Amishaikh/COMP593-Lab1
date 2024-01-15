e7d0bab454589c64e75d9347f4512c8459562f55
Amishaikh <amishai@flemingcollege.ca>
2024-01-15 13:40:26 -0500

    Modified both files in clone Repo
    -Delete a script statement from script1.
    -Adding two comments to script 2.

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 #Lines from Zen of Python
 print("Special cases aren't special enough to break the rules.")
 print("In the face of ambiguity, refuse the temptation to guess.")
+print("If the implementation is hard to explain, it's a bad idea.")
